43c394d26eb52375ace6e804aa3e4bc7a4704e07
Nayan <nayan.jaiswal@coriolis.co.in>
2024-08-14 14:11:03 +0530

    added signup and login page

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from "@/features/auth/components/register-form";
+
+const Login = () => {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+};
+export default Login;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
-"use client";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/features/auth/components/login-button";
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
-  const session = useSession();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {!session.data ? (
-        <button onClick={() => signIn("github", { redirectTo: "/dashboard" })}>
-          Sign In
-        </button>
-      ) : (
-        <button onClick={() => signOut()}>Sign Out</button>
-      )}
-
-      {JSON.stringify(session)}
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service </p>
+      </div>
+      <LoginButton>
+        <Button variant="secondary" className="lg">
+          Sign in
+        </Button>
+      </LoginButton>
     </main>
   );
 }
